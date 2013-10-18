6b351e03afac9249107ccca56c3628f8a30536a0
GroupFive <stephen.aggor@ashesi.edu.gh>
2013-10-18 16:24:45 +0000

    First initializations
    to test the GitHub

---
+++ src/gitlab/GitLab.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitlab;
+
+/**
+ *
+ * @author George S N Neequaye
+ */
+public class GitLab {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
